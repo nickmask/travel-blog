85bfd39aed4ec97828befefb3392824695a7db18
Nick Maskell <nrmaskell@gmail.com>
2019-01-22 22:22:02 +1300

    Fix app config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ try {
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
 }
 
 const { spaceId, accessToken } = contentfulConfig
@@ -20,7 +21,7 @@ if (!spaceId || !accessToken) {
 }
 
 module.exports = {
-  pathPrefix: '/gatsby-contentful-starter',
+  pathPrefix: '/travel-blog',
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
@@ -29,6 +30,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
-    }
+    },
   ],
 }
